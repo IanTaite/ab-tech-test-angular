205281a3089f4574fa9a165095cdcec82fdde5e4
Ian Taite <ian.taite@gmail.com>
2023-03-21 15:04:12 +0000

    Fix bug computing cart grand total.

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -10,23 +10,22 @@ import {
 export const reducer = createReducer(
   initialState,
   on(addProductToCart, (state, action) => {
-    const exists = state.cart.items.find((item) => item.sku === action.sku);
+    const {sku, title, unitPrice, thumbnail } = action;
+    const exists = state.cart.items.find((item) => item.sku === sku);
     if (exists) { // update item
-      return {
+      const newCartItems = state.cart.items.map((item) => item.sku === sku ? { ...item, quantity: item.quantity + 1, subTotal: item.unitPrice * (item.quantity + 1) } : item );
+      const newGrandTotal = newCartItems.reduce((total, item) => total + (item.subTotal), 0);
+        return {
         ...state,
         cart: {
           ...state.cart,
-          items: state.cart.items.map((item) =>
-            item.sku === action.sku
-            ? { ...item, quantity: item.quantity + 1, subTotal: item.unitPrice * (item.quantity + 1) }
-            : item
-          )
+          grandTotal: newGrandTotal,
+          items: newCartItems,
         },
       };
     } else { // add item
-      const {sku, title, unitPrice, thumbnail } = action;
       const newCartItems = [...state.cart.items, { sku, title, unitPrice, quantity: 1, subTotal: unitPrice, thumbnail } ];
-      const newGrandTotal = newCartItems.reduce((total, item) => total + item.unitPrice, 0);
+      const newGrandTotal = newCartItems.reduce((total, item) => total + item.subTotal, 0);
       return {
         ...state,
         cart: {
@@ -43,7 +42,7 @@ export const reducer = createReducer(
       return state;
     }
     const newCartItems = state.cart.items.filter((item) => item !== existingItem);
-    const newGrandTotal = newCartItems.reduce((total, item) => total + item.unitPrice, 0);
+    const newGrandTotal = newCartItems.reduce((total, item) => total + item.subTotal, 0);
     return {
       ...state,
       cart: {
@@ -55,7 +54,7 @@ export const reducer = createReducer(
   }),
   on(incrementProductQuantity, (state, action) => {
     const newCartItems = state.cart.items.map((item) => item.sku === action.sku ? { ...item, quantity: item.quantity + 1, subTotal: item.unitPrice * (item.quantity + 1) } : item );
-    const newGrandTotal = newCartItems.reduce((total, item) => total + (item.unitPrice * item.quantity), 0);
+    const newGrandTotal = newCartItems.reduce((total, item) => total + (item.subTotal), 0);
     return {
       ...state,
       cart: {
@@ -69,7 +68,7 @@ export const reducer = createReducer(
     const newCartItems = state.cart.items
       .map((item) => item.sku === action.sku ? { ...item, quantity: item.quantity - 1, subTotal: item.unitPrice * (item.quantity - 1) } : item )
       .filter((item) => item.subTotal > 0);
-    const newGrandTotal = newCartItems.reduce((total, item) => total + (item.unitPrice * item.quantity), 0);
+    const newGrandTotal = newCartItems.reduce((total, item) => total + item.subTotal, 0);
     return {
       ...state,
       cart: {
